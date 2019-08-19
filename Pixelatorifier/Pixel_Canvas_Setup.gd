aaa736195152c406afc4c20cc474755fd6861d38
Unknown <bergapie@gmail.com>
2019-08-19 18:02:24 +0200

    Daft fix 1.0
    1. Almond rolling.
    2. Code clean.
    3. Camera adjust.

--- Pixelatorifier/Pixel_Canvas_Setup.gd
+++ Pixelatorifier/Pixel_Canvas_Setup.gd
@@ -10,5 +10,4 @@ func _process(delta):
 	margin_bottom = get_viewport_rect().size.y
 	
 	pixel_clamp = get_child(0).value
-	print(pixel_clamp)
 	self.get_material().set_shader_param("pixel_clamp", pixel_clamp)
