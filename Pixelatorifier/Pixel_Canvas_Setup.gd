e0eee9b711e1ae413786dffe49590b97ac79cfc4
Unknown <bergapie@gmail.com>
2019-08-19 17:45:25 +0200

    Version 0.0
    Almond in the light

---
+++ Pixelatorifier/Pixel_Canvas_Setup.gd
@@ -0,0 +1,14 @@
+extends ColorRect
+
+var pixel_clamp
+
+func _ready():
+	pass
+
+func _process(delta):
+	margin_right = get_viewport_rect().size.x
+	margin_bottom = get_viewport_rect().size.y
+	
+	pixel_clamp = get_child(0).value
+	print(pixel_clamp)
+	self.get_material().set_shader_param("pixel_clamp", pixel_clamp)
---
+++ Scripts/Alm_bearer.gd
@@ -0,0 +1,28 @@
+extends RigidBody
+
+const MOVE_SPEED = 10
+var ortho
+var persp
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	ortho = $"./Orthogonal"
+	persp = $"./Perspective"
+
+func _process(delta):
+	if Input.is_action_pressed("forward"):
+		translate(Vector3(0,0,-MOVE_SPEED * delta))
+	if Input.is_action_pressed("backward"):
+		translate(Vector3(0,0,MOVE_SPEED * delta))
+	if Input.is_action_pressed("left"):
+		translate(Vector3(-MOVE_SPEED * delta,0,0))
+	if Input.is_action_pressed("right"):
+		translate(Vector3(MOVE_SPEED * delta,0,0))
+		
+	if Input.is_action_just_pressed("change_camera"):
+		if ortho.current == true:
+			persp.current = true
+			ortho.current = false
+		else:
+			persp.current = false
+			ortho.current = true
