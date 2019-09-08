8cdb35178a208e10b1dd11780d32ed3e8a823405
Phillip <bergapie@gmail.com>
2019-09-08 14:50:37 +0200

    Aspect ratio scaling
    Added aspect ratio scaling to pixel grid. Aspect ratio is sent from script to shader.

--- Pixelatorifier/Pixel_Canvas_Setup.gd
+++ Pixelatorifier/Pixel_Canvas_Setup.gd
@@ -1,6 +1,7 @@
 extends ColorRect
 
 var pixel_clamp
+var screen_size
 
 func _ready():
 	pass
@@ -9,5 +10,8 @@ func _process(delta):
 	margin_right = get_viewport_rect().size.x
 	margin_bottom = get_viewport_rect().size.y
 	
+	screen_size = get_viewport_rect().size
 	pixel_clamp = get_child(0).value
+	print(screen_size.x/screen_size.y)
+	self.get_material().set_shader_param("aspect_ratio", screen_size.x/screen_size.y)
 	self.get_material().set_shader_param("pixel_clamp", pixel_clamp)
--- Pixelatorifier/Pixel_Grid.shader
+++ Pixelatorifier/Pixel_Grid.shader
@@ -1,13 +1,18 @@
 shader_type canvas_item;
 
+uniform float aspect_ratio; // X base
 uniform float pixel_clamp = 0.003;
 
 void fragment() 
 {
+	float x = pixel_clamp / aspect_ratio;
+	float y = pixel_clamp;
+	
 	vec2 uv = SCREEN_UV;
-	vec2 muv = mod(uv, pixel_clamp);
-	if (muv.x <= 0.001 || muv.y <=0.001)
+	vec2 muv = mod(uv, vec2(x, y));
+	if (muv.x <= 0.002 || muv.y <=0.002)
 		COLOR.rgb = vec3(0,0,0);
 	else
 		COLOR.rgb = textureLod(SCREEN_TEXTURE,uv,0).rgb;
+		
 }
