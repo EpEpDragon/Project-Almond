ca39c1643ebf1e204bfcf019e72db94d015f8cf8
Phillip <bergapie@gmail.com>
2019-09-08 15:48:18 +0200

    WIP Shugar Rush: Shift sample points
    Shift pixel sample points based on camera position. Shifts are too large.

--- Pixelatorifier/Pixel_Canvas_Setup.gd
+++ Pixelatorifier/Pixel_Canvas_Setup.gd
@@ -2,9 +2,10 @@ extends ColorRect
 
 var pixel_clamp
 var screen_size
+var cam_perspective
 
 func _ready():
-	pass
+	cam_perspective = get_tree().get_root().get_node("Spatial/Player")
 
 func _process(delta):
 	margin_right = get_viewport_rect().size.x
@@ -12,6 +13,6 @@ func _process(delta):
 	
 	screen_size = get_viewport_rect().size
 	pixel_clamp = get_child(0).value
-	print(screen_size.x/screen_size.y)
+	self.get_material().set_shader_param("pos", Vector2(cam_perspective.translation.x, cam_perspective.translation.z))
 	self.get_material().set_shader_param("aspect_ratio", screen_size.x/screen_size.y)
 	self.get_material().set_shader_param("pixel_clamp", pixel_clamp)
--- Pixelatorifier/Pixel_Grid.shader
+++ Pixelatorifier/Pixel_Grid.shader
@@ -1,5 +1,6 @@
 shader_type canvas_item;
 
+uniform vec2 pos;
 uniform float aspect_ratio; // X base
 uniform float pixel_clamp = 0.003;
 
@@ -8,11 +9,15 @@ void fragment()
 	float x = pixel_clamp / aspect_ratio;
 	float y = pixel_clamp;
 	
+	vec2 offset = mod(pos, vec2(x,y));
 	vec2 uv = SCREEN_UV;
+	
 	vec2 muv = mod(uv, vec2(x, y));
-	if (muv.x <= 0.002 || muv.y <=0.002)
-		COLOR.rgb = vec3(0,0,0);
-	else
-		COLOR.rgb = textureLod(SCREEN_TEXTURE,uv,0).rgb;
-		
+	
+	COLOR.rgb = textureLod(SCREEN_TEXTURE,uv,0).rgb;
+
+	uv -= offset*10f;
+	muv = mod(uv, vec2(x, y));
+	if (muv.x <= 0.001 || muv.y <= 0.001)
+		COLOR.rgb = vec3(1,1,1);
 }
