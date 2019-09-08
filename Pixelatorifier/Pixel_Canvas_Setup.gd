31eb3f9772c34d651d320b73c5de78eb9db41475
Phillip <bergapie@gmail.com>
2019-09-08 21:21:51 +0200

    Any aspect ratio support
    Works with any aspect ratio

--- Pixelatorifier/Pixel_Canvas_Setup.gd
+++ Pixelatorifier/Pixel_Canvas_Setup.gd
@@ -1,6 +1,5 @@
 extends ColorRect
 
-var pixel_clamp
 var screen_size
 var cam_perspective
 var ortho_size
@@ -14,7 +13,16 @@ func _process(delta):
 	margin_bottom = get_viewport_rect().size.y
 	
 	screen_size = get_viewport_rect().size
-	pixel_clamp = get_child(0).value
 	self.get_material().set_shader_param("pos", Vector2(cam_perspective.translation.x, cam_perspective.translation.z)/ortho_size)
 	self.get_material().set_shader_param("aspect_ratio", screen_size.x/screen_size.y)
-	self.get_material().set_shader_param("pixel_clamp", pixel_clamp)
+	
+
+func _on_CheckButton_toggled(button_pressed):
+	if !(button_pressed):
+		self.get_material().set_shader_param("grid", false)
+	else:
+		self.get_material().set_shader_param("grid", true)
+
+
+func _on_HScrollBar_value_changed(value):
+	self.get_material().set_shader_param("pixel_clamp", value)
--- Pixelatorifier/Pixel_Grid.shader
+++ Pixelatorifier/Pixel_Grid.shader
@@ -1,26 +1,27 @@
 shader_type canvas_item;
 
+uniform bool grid = false;
 uniform vec2 pos;
 uniform float aspect_ratio; // X base
-uniform float pixel_clamp = 0.003;
+uniform float pixel_clamp = 0.7;
 
 void fragment() 
 {
 	float x = pixel_clamp / aspect_ratio;
 	float y = pixel_clamp;
 	
-	vec2 offset = mod(pos, vec2(x,y));
+	vec2 offset = mod(vec2(pos.x/aspect_ratio, pos.y), vec2(x,y));
 	vec2 uv = SCREEN_UV;
 	
 	vec2 muv = mod(uv, vec2(x, y));
+
 	uv -= mod(uv,vec2(x,y));
 	//-mod(uv,vec2(x,y))
-
-
 	
 	uv.y += offset.y;
 	uv.x -= offset.x;
 	COLOR.rgb = textureLod(SCREEN_TEXTURE,uv,0).rgb;
-	if (muv.x <= 0.001 || muv.y <= 0.001)
-		COLOR.rgb = vec3(1,1,1);
+	if (grid)
+		if (muv.x <= 0.001 || muv.y <= 0.001)
+			COLOR.rgb = vec3(1,1,1);
 }
