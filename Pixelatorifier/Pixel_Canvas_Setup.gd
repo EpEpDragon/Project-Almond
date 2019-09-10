0f908959bd6b9ea9b1af485c5c78f7f038a1041c
Phillip <bergapie@gmail.com>
2019-09-10 10:14:33 +0200

    Code to adjust for camera rotation
    Only works with X rotation, need to use different rotation get method.

--- Pixelatorifier/Pixel_Canvas_Setup.gd
+++ Pixelatorifier/Pixel_Canvas_Setup.gd
@@ -2,18 +2,19 @@ extends ColorRect
 
 var screen_size
 var cam_perspective
-var ortho_size
-
+var ortho_cam
+var rot_adjustment
 func _ready():
 	cam_perspective = get_tree().get_root().get_node("Spatial/Orthogonal")
-	ortho_size = get_tree().get_root().get_node("Spatial/Orthogonal").size
+	ortho_cam = get_tree().get_root().get_node("Spatial/Orthogonal")
+	rot_adjustment = Vector2(sin(PI/2-ortho_cam.rotation.y),cos(-PI/2-ortho_cam.rotation.x)) #Adjust shader input for ortho camera rotation
 
 func _process(delta):
 	margin_right = get_viewport_rect().size.x
 	margin_bottom = get_viewport_rect().size.y
 	
 	screen_size = get_viewport_rect().size
-	self.get_material().set_shader_param("pos", Vector2(cam_perspective.translation.x, cam_perspective.translation.z)/ortho_size)
+	self.get_material().set_shader_param("pos", Vector2(cam_perspective.translation.x, cam_perspective.translation.z)*rot_adjustment/ortho_cam.size)
 	self.get_material().set_shader_param("aspect_ratio", screen_size.x/screen_size.y)
 	
 
