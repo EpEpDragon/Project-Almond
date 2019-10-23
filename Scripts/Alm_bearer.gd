358c8db5af2b9e31de5952d07c9f5805d7bb480e
Phillip <bergapie@gmail.com>
2019-10-23 18:50:16 +0200

    Rotating Almond
    The almond may now face it's problems

--- Scripts/Alm_bearer.gd
+++ Scripts/Alm_bearer.gd
@@ -4,22 +4,40 @@ const MOVE_SPEED = 10
 const SNAP = 1
 var ortho
 var persp
+var ray_origin
+var ray_target
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	ortho = $"./Orthogonal"
-	persp = $"./Perspective"
+	persp = get_tree().get_root().get_node("Spatial/Perspective")
 
+func _input(event):
+	if event is InputEventMouseMotion:
+		ray_origin = persp.project_ray_origin(get_viewport().get_mouse_position())
+		ray_target = persp.project_ray_normal(get_viewport().get_mouse_position())*500
+		print("pll")
+		
+func _physics_process(delta):
+	var space_state = get_world().direct_space_state
+	var ray = space_state.intersect_ray(ray_origin, ray_target,[self],1,true,true)
+	if ray:
+		look_at(Vector3(ray.position.x,translation.y,ray.position.z), Vector3(0,1,0))
+		
 func _process(delta):
 	if Input.is_action_pressed("forward"):
-		translate(Vector3(0,0,-MOVE_SPEED * delta))
+		global_translate(Vector3(0,0,-MOVE_SPEED * delta))
 	if Input.is_action_pressed("backward"):
-		translate(Vector3(0,0,MOVE_SPEED * delta))
+		global_translate(Vector3(0,0,MOVE_SPEED * delta))
 	if Input.is_action_pressed("left"):
-		translate(Vector3(-MOVE_SPEED * delta,0,0))
+		global_translate(Vector3(-MOVE_SPEED * delta,0,0))
 	if Input.is_action_pressed("right"):
-		translate(Vector3(MOVE_SPEED * delta,0,0))
-
+		global_translate(Vector3(MOVE_SPEED * delta,0,0))
+	if Input.is_action_just_pressed("cameraFollow"):
+		if persp.follow == true:
+			persp.follow = false
+		else:
+			persp.follow = true
+		
 	if Input.is_action_just_pressed("change_camera"):
 		if ortho.current == true:
 			persp.current = true
@@ -27,3 +45,4 @@ func _process(delta):
 		else:
 			persp.current = false
 			ortho.current = true
+
--- Scripts/Perspective.gd
+++ Scripts/Perspective.gd
@@ -1,26 +1,27 @@
 extends Camera
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 const MOVE_SPEED = 10
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-	#pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+var follow = true;
+var player
+func _ready():
+	player = get_tree().get_root().get_node("Spatial/Player")
+	
 func _process(delta):
-	if Input.is_action_pressed("camRight"):
-		global_translate(Vector3(MOVE_SPEED * delta,0,0))
-	if Input.is_action_pressed("camLeft"):
-		global_translate(Vector3(-MOVE_SPEED * delta,0,0))
-	if Input.is_action_pressed("camForward"):
-		global_translate(Vector3(0,0,-MOVE_SPEED * delta))
-	if Input.is_action_pressed("camBackward"):
-		global_translate(Vector3(0,0,MOVE_SPEED * delta))
-	if Input.is_action_pressed("camUp"):
-		global_translate(Vector3(0,MOVE_SPEED * delta,0))
-	if Input.is_action_pressed("camDown"):
-		global_translate(Vector3(0,-MOVE_SPEED * delta,0))
+	if follow:
+		translation.x = player.translation.x
+		translation.z = player.translation.z + 20
+	else:
+		if Input.is_action_pressed("camRight"):
+			global_translate(Vector3(MOVE_SPEED * delta,0,0))
+		if Input.is_action_pressed("camLeft"):
+			global_translate(Vector3(-MOVE_SPEED * delta,0,0))
+		if Input.is_action_pressed("camForward"):
+			global_translate(Vector3(0,0,-MOVE_SPEED * delta))
+		if Input.is_action_pressed("camBackward"):
+			global_translate(Vector3(0,0,MOVE_SPEED * delta))
+		if Input.is_action_pressed("camUp"):
+			global_translate(Vector3(0,MOVE_SPEED * delta,0))
+		if Input.is_action_pressed("camDown"):
+			global_translate(Vector3(0,-MOVE_SPEED * delta,0))
 	
 	
