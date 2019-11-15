00310d17fbe9e6a69f925098b1a9554ae96184eb
Phillip <bergapie@gmail.com>
2019-11-15 13:19:05 +0200

    Zone assignment system
    Basic system to randomly assign zones to BSP

--- WorldGen/WorldGenTesting.gd
+++ WorldGen/WorldGenTesting.gd
@@ -1,33 +1,49 @@
 extends Node2D 
 var world_root = Vector2(0,0)
-var world_size = Vector2(1300,1300)
-var max_room_size = 100000;
+var world_size = Vector2(1900,1400)
+var max_room_size = 150000;
 var min_room_size = 5000;
 var world = []
 var rand = RandomNumberGenerator.new()
 var chunkDraw = 0;
-var e = 2.718281828
 var vec1 = Vector2(1,1)
+var tree_size = 0
+
+var drawMode = "type"
 
 func _ready():
 	
 	rand.seed = 34545
+	build()
+	print("fin")
+
+func build():
 	world.resize(1)
+	tree_size = 0
 	
 	#Set world root node
 	world[0] = {
 		origin = world_root,
-		size = world_size
+		size = world_size,
+		type = "overworld"
 		}
 	
 	bsp(world_root, world_size, 0, 0)
-	print("World Finished")
-	
-	
+	assign_nodes(0)
+	return
+
 func bsp(origin : Vector2, size : Vector2, depth : int, node : int):
 	var current_size = size.x*size.y
-	var roll = log(-max_room_size/current_size + 1)
-	if current_size <= max_room_size && rand.randf() > log((current_size/max_room_size)*5 + 1):
+		
+	#Threshold to continue division for smaller than max size rooms
+	var threshold = log((current_size/max_room_size)*5 + 1)
+	
+	if tree_size < node:
+		tree_size = node
+	
+	if threshold > 0.9:
+		threshold = 0.9
+	if current_size <= max_room_size && rand.randf() > threshold:
 		return
 	else:
 		var left_origin
@@ -39,7 +55,7 @@ func bsp(origin : Vector2, size : Vector2, depth : int, node : int):
 		#Split % based on seed
 		var split = rand.randf_range(0.3,0.7)
 		
-		if(fmod(depth, 2) == 0):
+		if (fmod(depth, 2) == 0):
 			#Vertical split
 			left_origin = origin
 			left_size = size - Vector2(size.x*(1-split),0)
@@ -61,17 +77,38 @@ func bsp(origin : Vector2, size : Vector2, depth : int, node : int):
 		#Add right node
 		world[node*2+2] = {
 			origin = right_origin,
-			size = right_size
+			size = right_size,
+			type = "chunk"
 			}
 		#Add left node
 		world[node*2+1] = {
 				origin = left_origin,
-				size = left_size
+				size = left_size,
+				type = "chunk"
 				}
 		#BSP for left and right
 		bsp(left_origin, left_size, depth+1, node*2+1)
 		bsp(right_origin, right_size, depth+1, node*2+2)
 		return
+
+func assign_nodes(node : int):
+	var children = 0;
+	if node*2+1 <= tree_size: 
+		if world[node*2+1]:
+			assign_nodes(node*2+1)
+			children += 1
+	if node*2+2 <= tree_size: 
+		if world[node*2+2]:
+			assign_nodes(node*2+2)
+			children += 1
+	if children == 0:
+		if rand.randf() < 0.6:
+			world[node].type = "cave"
+		elif rand.randf() < 0.8:
+			world[node].type = "fill"
+		else:
+			world[node].type = "structure"
+	return
 
 func _input(event):
 	if event.is_action_released("drawNextChunk"):
@@ -81,13 +118,24 @@ func _input(event):
 		chunkDraw -= 1
 		update()
 	if event.is_action_released("newMap"):
-		world.resize(1);
-		bsp(world_root, world_size, 0, 0)
+		build()
 		update()
 		print("NBew")
+	if event.is_action_released("drawMode"):
+		if drawMode == "bsp":
+			drawMode = "type"
+		else:
+			drawMode = "bsp"
+		update()
 
 func _draw():
 	for chunk in world:
 		if chunk:
-			draw_rect(Rect2(chunk.origin + vec1, chunk.size + vec1), Color(1,0,0), false)
-
+			#draw_rect(Rect2(chunk.origin + vec1, chunk.size + vec1), Color(1,0,0), false)
+			if drawMode == "type":
+				if chunk.type == "cave":
+					draw_rect(Rect2(chunk.origin + vec1, chunk.size + vec1), Color(0,0,1))
+				elif chunk.type == "structure":
+					draw_rect(Rect2(chunk.origin + vec1, chunk.size + vec1), Color(0,1,0))
+				else:
+					draw_rect(Rect2(chunk.origin + vec1, chunk.size + vec1), Color(0,0,0))
